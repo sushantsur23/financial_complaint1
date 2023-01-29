9cfa85e20007657eb38b8b2b12f6d9d431391606
sushantsur23 <sushant.sur23@gmail.com>
2023-01-29 23:11:35 +0530

    basic structure  and requirements updated

---
+++ main.py
@@ -0,0 +1,41 @@
+import os
+from finance_complaint.constant.environment.variable_key import AWS_ACCESS_KEY_ID_ENV_KEY,AWS_SECRET_ACCESS_KEY_ENV_KEY
+from dotenv import load_dotenv
+load_dotenv()
+access_key_id = os.getenv(AWS_ACCESS_KEY_ID_ENV_KEY, )
+secret_access_key = os.getenv(AWS_SECRET_ACCESS_KEY_ENV_KEY, )
+#print(access_key_id,secret_access_key)
+import argparse
+from finance_complaint.exception import FinanceException
+from finance_complaint.pipeline import TrainingPipeline, PredictionPipeline
+# from finance_complaint.logger import logger
+# from finance_complaint.config.pipeline.training import FinanceConfig
+import sys
+
+
+
+def start_training(start=False):
+    try:
+        if not start:
+            return None
+        print("Training Running")
+        TrainingPipeline(FinanceConfig()).start()
+        
+    except Exception as e:
+        raise FinanceException(e, sys)
+
+
+
+if __name__ == "__main__":
+    try:
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--t", default=0, type=int, help="If provided true training will be done else not")
+        parser.add_argument("--p", default=0, type=int, help="If provided prediction will be done else not")
+
+        args = parser.parse_args()
+
+        main(training_status=args.t, prediction_status=args.p)
+    except Exception as e:
+        print(e)
+        pass
+        logger.exception(FinanceException(e, sys))
---
+++ start.sh
@@ -0,0 +1,1 @@
+python3 main.py --t $TRAINING --p $PREDICTION
